9b23b898da27e524e549c2ab444953c3f3cba00c
hojeong <>
2020-09-07 10:04:02 +0900

    hello

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,13 +2,8 @@
 
 int main(void)
 {
-    int input;
-    int result=0;
-    for(int i=0; i<5; i++){
-        std::cout<<i+1<<"번째 정수 입력: ";
-        std::cin>>input;
-        result+=input;
-    }
-    std::cout<<"합계: "<<result;
+    std::cout<<"Hello World"<<std::endl;
+    
+    system("pause");
     return 0;
 }
