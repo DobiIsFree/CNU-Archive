ef2d13b83905e22bbf3d1e9186294c8838a8739a
hojeong <>
2020-09-21 15:15:33 +0900

    oop04

--- W04/homework1.c
+++ W04/homework1.c
@@ -4,7 +4,7 @@ int main(){
    char s[] = "";
     printf("문자를 입력하세요(100자 이내).\n");
     scanf("%s", s);
-    printf("입력된 문자는 %s 입니다.", s);
-    
+    printf("입력된 문자는 %s 입니다.\n", s);
+
     return 0;
 }
